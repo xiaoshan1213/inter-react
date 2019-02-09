a7b0dca803bc30f4163d0e4fbf74b435edaefac7
Wenjin Ma <wenjin@Wenjins-MacBook-Pro.local>
2019-02-09 11:28:29 -0800

    Updates

---
+++ precache-manifest.14e7670ba99a029c94776e538eb42828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f78a6b9adcaa93c65202",
+    "url": "/inter-react/static/css/main.1e3e9054.chunk.css"
+  },
+  {
+    "revision": "f78a6b9adcaa93c65202",
+    "url": "/inter-react/static/js/main.f78a6b9a.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/inter-react/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "849cf457db48ac362127",
+    "url": "/inter-react/static/js/runtime~main.849cf457.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inter-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd64b7f505502814b759605f88b14eae",
+    "url": "/inter-react/index.html"
+  }
+];
