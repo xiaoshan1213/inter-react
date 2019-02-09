7978eb75ea68cb7c932c288449f378479f9a5846
Wenjin Ma <wenjin@Wenjins-MacBook-Pro.local>
2019-02-09 15:57:00 -0800

    Updates

---
+++ precache-manifest.0c67ba9c57e4c745b633b1aec35ffdef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9835f179edf3caa8116",
+    "url": "/inter-react/static/css/main.885d6d7a.chunk.css"
+  },
+  {
+    "revision": "f9835f179edf3caa8116",
+    "url": "/inter-react/static/js/main.f9835f17.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/inter-react/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "849cf457db48ac362127",
+    "url": "/inter-react/static/js/runtime~main.849cf457.js"
+  },
+  {
+    "revision": "0c26685f11cac36831ad35c5489586eb",
+    "url": "/inter-react/static/media/demo-bg.0c26685f.jpg"
+  },
+  {
+    "revision": "7fa6111cf4e61a5b747b51e458878625",
+    "url": "/inter-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26533d1ea961c43fc54df61ca17be352.js"
+  "/inter-react/precache-manifest.0c67ba9c57e4c745b633b1aec35ffdef.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/inter-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
