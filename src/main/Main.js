ba24355b2693eb5a58b8f9ad85d9d6b1b3cb1301
Wenjin Ma <wenjin@Wenjins-MacBook-Pro.local>
2019-02-10 20:57:32 -0800

    add main component

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,13 +4,19 @@ import './Main.css';
 class Main extends Component {
     render () {
         return (
-            <div></div>
+            <div className="container demo">
+                <div className="content">
+                    <div id="large-header" className="large-header">
+                    <canvas id="demo-canvas"></canvas>
+                    <h1 className="main-title"><span className="thin">Coming Soon</span></h1>
+                    </div>
+                </div>
+            </div>
         );
     }
 
     componentDidMount(){
-        console.log("mounted");
-        document.getElementById('large-header').style.height = 200+"px";
+        
     }
     
 }
